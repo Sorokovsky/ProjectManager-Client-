34998afe18dac8f6eb502f81840856efb7208bf0
Sorokovsky <Sorokovskys@ukr.net>
2022-11-05 17:12:27 +0300

    header

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,8 +4,11 @@ import {indexMeta} from "../data/neta";
 import logo from '../asssets/logo.svg';
 import styles from '../styles/Logo.module.sass';
 const Logo:React.FC = ():JSX.Element => {
+    const clickHandler = React.useCallback((e:React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+    }, []);
     return (
-        <Link href={'/'} className={styles.logo}>
+        <Link href={'/'} onClick={clickHandler} className={styles.logo}>
             <img src={logo.src} alt={indexMeta.description}/>
         </Link>
     )
