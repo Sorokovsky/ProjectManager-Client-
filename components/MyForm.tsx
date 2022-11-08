03137561e67e0f93263b2f5bdd8a9ab08e244a57
Sorokovsky <Sorokovskys@ukr.net>
2022-11-08 22:04:08 +0300

    start reg form send and will style

--- components/MyForm.tsx
+++ components/MyForm.tsx
@@ -1,10 +1,21 @@
 import React, {HTMLAttributes} from "react";
+import styles from '../styles/MyForm.module.sass';
 interface iProps extends HTMLAttributes<HTMLFormElement>{
     children:React.ReactNode;
+    sendTo?:string;
+    onSubmit?: (e:React.FormEvent) => void;
 }
-const MyForm:React.FC<iProps> = ({children, className, ...props}):JSX.Element => {
+const MyForm:React.FC<iProps> = ({children,
+                                  className,
+                                  sendTo = "/",
+                                  onSubmit,
+                                  ...props}):JSX.Element => {
+    const form = React.useRef<HTMLFormElement>(null);
+    const sendHandler = onSubmit !== undefined ? onSubmit : (e:React.FormEvent) => {
+
+    }
     return (
-        <form action="#" className={[className].join(" ")} {...props}>
+        <form action="#" ref={form} className={[styles.form].join(" ")} {...props}>
             {children}
         </form>
     )
--- pages/registration.tsx
+++ pages/registration.tsx
@@ -2,11 +2,15 @@ import {NextPage} from "next";
 import React from "react";
 import MainLayout from "../components/MainLayout";
 import {registrationMeta} from "../data/meta";
+import styles from '../styles/Registration.module.sass';
+import MyForm from "../components/MyForm";
 const Registration:NextPage = ():JSX.Element => {
     return (
         <MainLayout meta={registrationMeta}>
-            <div className="container">
-
+            <div className={["container", styles.container].join(" ")}>
+                <MyForm>
+                    <h1>Зареєструватися</h1>
+                </MyForm>
             </div>
         </MainLayout>
     )
